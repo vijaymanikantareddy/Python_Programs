fd6b011f77e21d6cecb27f47196c1657d27f2664
vijaymanikantareddy <83952736+vijaymanikantareddy@users.noreply.github.com>
2021-06-16 07:59:22 +0530

    Create Pattern_i=j.py

---
+++ Pattern_i=j.py
@@ -0,0 +1,12 @@
+n=int(input('enter the number: '))
+for i in range(1,n+1):
+	print('\n')
+	for j in range(1,i+1):
+		print('*', end=' ')
+    
+#output
+#enter the number: 4
+# *
+# * *
+# * * *
+# * * * *
