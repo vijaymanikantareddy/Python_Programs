14e8736c0a9d14c7ac618273562167685805af85
vijaymanikantareddy <83952736+vijaymanikantareddy@users.noreply.github.com>
2021-06-16 13:41:39 +0530

    Update Fibonacci_series_in_single_line.py

--- Fibonacci_series_in_single_line.py
+++ Fibonacci_series_in_single_line.py
@@ -1,10 +1,16 @@
 pt=0
 ct=1
 n=int(input('enter a number: '))
-print(pt, end=' ')
-print(ct, end=' ')
-for i in range(3, n+1):
-    next= ct+pt
-    pt=ct
-    ct=next
-    print(next, end=' ')
+if n==1:
+    print(pt)
+elif n==2:
+    print(pt, end=' ')
+    print(ct, end=' ')
+else:    
+    print(pt, end=' ')
+    print(ct, end=' ')
+    for i in range(3, n+1):
+        next= ct+pt
+        pt=ct
+        ct=next
+        print(next, end=' ')
