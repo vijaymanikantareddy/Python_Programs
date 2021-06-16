7fbc89f081e9470eb6a80dff644699033c5a248a
vijaymanikantareddy <83952736+vijaymanikantareddy@users.noreply.github.com>
2021-06-16 13:40:37 +0530

    Update Fibonacci_series.py

--- Fibonacci_series.py
+++ Fibonacci_series.py
@@ -1,10 +1,16 @@
 pt=0
 ct=1
 n=int(input('enter a number: '))
-print(pt)
-print(ct)
-for i in range(3, n+1):
-    next= ct+pt
-    pt=ct
-    ct=next
-    print(next)
+if n==1:
+    print(pt)
+elif n==2:
+    print(pt)
+    print(ct)
+else:    
+    print(pt)
+    print(ct)
+    for i in range(3, n+1):
+        next= ct+pt
+        pt=ct
+        ct=next
+        print(next)
