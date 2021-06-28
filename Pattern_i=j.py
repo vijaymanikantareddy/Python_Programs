88d7aed0476dd7df96159a8c910460ede463918c
vijaymanikantareddy <83952736+vijaymanikantareddy@users.noreply.github.com>
2021-06-28 20:32:50 +0530

    Update Pattern_i=j.py

--- Pattern_i=j.py
+++ Pattern_i=j.py
@@ -1,9 +1,14 @@
 n=int(input('enter the number: '))
+#case-1
 for i in range(1,n+1):
 	print('\n')
 	for j in range(1,i+1):
 		print('*', end=' ')
-    
+		
+#case-2
+for i in range(0, n+1):
+    print((i+1)*"* ")
+
 #output
 #enter the number: 4
 # *
