88606e1eee1b4b1d948d079e79a03f24f252b746
vijaymanikantareddy <83952736+vijaymanikantareddy@users.noreply.github.com>
2021-09-27 18:08:14 +0530

    Update palindrome_or_not.py

--- Sets/palindrome_or_not.py
+++ Sets/palindrome_or_not.py
@@ -1,6 +1,20 @@
+flag=0
 s=input('enter a string: ')
+
+#using slicing
 a = s[::-1]
 if s==a:
+    print('string is palindrome')
+else:
+    print('string is not palindrome')
+    
+#using loop
+for i in range(len(s)):
+    if s[i]==s[len(s)-i-1]:
+        flag+=1
+    else:
+        break
+if flag==len(s):
     print('string is palindrome')
 else:
     print('string is not palindrome')
