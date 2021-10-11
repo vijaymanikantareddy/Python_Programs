33df8867ea79524326bcc8c9ccf04cdd4f9103bf
vijaymanikantareddy <83952736+vijaymanikantareddy@users.noreply.github.com>
2021-10-11 22:08:55 +0530

    Create single_linked_list_creation.py

---
+++ Data Structures/single_linked_list_creation.py
@@ -0,0 +1,31 @@
+class node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class SLL:
+    def __init__(self):
+        self.head = None
+    def display(self):
+        temp = self.head
+        if self.head is None:
+            print('List is empty')
+        else:
+            while temp:
+                print(temp.data, end=' ')
+                temp = temp.next
+
+L = SLL()
+val = int(input('enter data or -1 to stop: '))
+while val != -1:
+    n = node(val)
+    if L.head == None:
+        L.head = n
+        n.next = None
+    else:
+        temp = L.head
+        while temp.next != None:
+            temp = temp.next
+        temp.next = n
+        n.next = None
+    val = int(input('enter data or -1 to stop: '))
